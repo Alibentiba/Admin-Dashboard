0705c095a3c07a15fd1a57a15e9b0fa5b93e2102
Alibentiba <99966047+Alibentiba@users.noreply.github.com>
2022-06-27 23:49:16 +0100

    add serch input

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { RiAlertLine } from 'react-icons/ri';
 import { FaPhoneAlt } from 'react-icons/fa';
-
+import { AiOutlineSearch,AiOutlineUser} from 'react-icons/ai';
+import { MdFavoriteBorder } from 'react-icons/md';
+import { FiShoppingCart } from 'react-icons/fi';
 import './Header.css'
 // import { Dropdown } from 'react-bootstrap/Dropdown';
 
@@ -32,6 +34,32 @@ const Header = () => {
               <h4>1-888-546-789</h4>
         </div>
        </div>
+       <div className='sarch'>
+        <input type="text" className='search-input' placeholder="Enter keywords to search..."/>
+        <button className='sarch-button'> <AiOutlineSearch className='search-icon'/></button>
+        </div>
+        <div className='Header-center-right'>
+          <p className='number-favorits'>0</p>
+        <MdFavoriteBorder className='Header-center-right-icon'/>
+        <p className='number-buy'>0</p>
+        <FiShoppingCart className='Header-center-right-icon'/>
+        <AiOutlineUser className='Header-center-right-icon'/>
+
+        <div className='Header-singin'>
+          <p>MY ACCOUNT</p>
+          <h6>Login/Register</h6>
+         
+
+        </div>
+
+
+
+        </div>
+
+
+
+
+
    </div>
 
 
