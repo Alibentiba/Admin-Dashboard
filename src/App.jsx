c003a3e1c21ca00d9d3e21e0c4b69b9c745fd23a
unknown <bentibaali3@gmail.com>
2022-06-29 15:47:35 +0100

    policy

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Baner from './Baner/Baner'
 import Header from './Header/Header'
+import Policy from "../src/Policy/Policy"
+import "../src/App.css"
 function App() {
   return (
     <div className='App'>
       <Header/>
       <Baner/>
+      <Policy/>
 
 
     </div>
